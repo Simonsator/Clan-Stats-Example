cd260f82d954e495b6d32a01ab780d4332e3b7cb
Simon Brungs <00pflaume@gmail.com>
2016-11-11 21:25:21 +0100

    Added comments

--- src/main/java/de/simonsator/clans/example/stats/StatsExample.java
+++ src/main/java/de/simonsator/clans/example/stats/StatsExample.java
@@ -2,7 +2,7 @@ package de.simonsator.clans.example.stats;
 
 import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
 import de.simonsator.partyandfriends.clan.api.Clan;
-import de.simonsator.partyandfriends.clan.api.ClanStats;
+import de.simonsator.partyandfriends.clan.api.ClanStat;
 import de.simonsator.partyandfriends.clan.commands.ClanCommands;
 import de.simonsator.partyandfriends.clan.commands.subcommands.Stats;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -11,12 +11,41 @@ import net.md_5.bungee.api.plugin.Plugin;
  * @author simonbrungs
  * @version 1.0.0 11.11.16
  */
-public class StatsExample extends Plugin implements ClanStats {
+public class StatsExample extends Plugin implements ClanStat {
+
+	/**
+	 * onEnable method. In this method the Clan stat gets registered.
+	 */
 	public void onEnable() {
-		((Stats) ClanCommands.getInstance().getSubCommand(Stats.class)).registerClanStats(this, this);
+		// Gets the instance of the main clan command (/clan)
+		ClanCommands clanCommandInstance = ClanCommands.getInstance();
+		// Gets the SubCommand which is an instance of the given class (in this case Stats (/clan stats))
+		Stats statCommand = ((Stats) clanCommandInstance.getSubCommand(Stats.class));
+		// registers this as a ClanStat, which should be called (stats(OnlinePAFPlayer pSender, Clan pClan)) if a player uses /clan stats.
+		statCommand.registerClanStats(this, this);
 	}
 
-	public void stats(OnlinePAFPlayer pSender, Clan pClan) {
+	/**
+	 * onDisable method. In this method the Clan stat gets unregistered (must not be done onDisable)
+	 */
+	public void onDisable() {
+		// Gets the instance of the main clan command (/clan)
+		ClanCommands clanCommandInstance = ClanCommands.getInstance();
+		// Gets the SubCommand which is an instance of the given class (in this case Stats (/clan stats))
+		Stats statCommand = ((Stats) clanCommandInstance.getSubCommand(Stats.class));
+		/*
+		* Unregisters all ClanStats which were registered by this plugin. The method
+		* stats(OnlinePAFPlayer pSender, Clan pClan) of this class will no longer be called if /clan stats get called by a player
+		*/
+		statCommand.unregisterAll(this);
+	}
 
+	/**
+	 * @param pSender The
+	 * @param pClan
+	 */
+	public void stats(OnlinePAFPlayer pSender, Clan pClan) {
+		// sends a message to the player who used /clan stats and tells him, how many people of this clan are online at the moment
+		pSender.sendMessage("From this clan are " + pClan.getAllOnlineClanPlayers() + " members online.");
 	}
 }
