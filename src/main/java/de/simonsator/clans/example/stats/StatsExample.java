55172cf74c939d0c07bc4bf7de17e178b8d827a8
Simon Brungs <00pflaume@gmail.com>
2016-11-12 22:26:40 +0100

    Corrected language mistakes in the comments

--- src/main/java/de/simonsator/clans/example/stats/StatsExample.java
+++ src/main/java/de/simonsator/clans/example/stats/StatsExample.java
@@ -23,7 +23,7 @@ public class StatsExample extends Plugin implements ClanStat {
 		// Gets the SubCommand which is an instance of the given class (in this case Stats (/clan stats)).
 		Stats statCommand = ((Stats) clanCommandInstance.getSubCommand(Stats.class));
 
-		// registers this as a ClanStat, which should be called (stats(OnlinePAFPlayer pSender, Clan pClan)) if a player uses /clan stats.
+		// registers this as a ClanStat which should be called (stats(OnlinePAFPlayer pSender, Clan pClan)) if a player uses /clan stats.
 		statCommand.registerClanStats(this, this);
 	}
 
@@ -39,21 +39,23 @@ public class StatsExample extends Plugin implements ClanStat {
 
 		/*
 		* Unregisters all ClanStats which were registered by this plugin. The method
-		* stats(OnlinePAFPlayer pSender, Clan pClan) of this class will no longer be called if /clan stats get called by a player.
+		* stats(OnlinePAFPlayer pSender, Clan pClan) of the current instance of this
+		* class will no longer be called if /clan stats is called by a player.
 		*/
 		statCommand.unregisterAll(this);
 	}
 
 	/**
 	 * This method gets executed when a player calls the command /clan stats <clan>
+	 *
 	 * @param pSender The player who executed /clan stats
-	 * @param pClan The clan of which the stats was requested (it c
+	 * @param pClan   The clan of which the stats was requested (it c
 	 */
 	public void stats(OnlinePAFPlayer pSender, Clan pClan) {
 		// Here belongs the code which should be executed when /clan stats is called.
 
 
-		// Sends a message to the player who used /clan stats and tells him, how many people of this clan are online at the moment
+		// Sends a message to the player who used /clan stats and tells him how many people of this clan are online at the moment.
 		pSender.sendMessage("From this clan are " + pClan.getAllOnlineClanPlayers() + " members online.");
 	}
 }
